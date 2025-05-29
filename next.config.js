1ff6b30bed606a2e2feb64be70077bfda40b20fd
kashifaalogics <kashif.sohail.aalogics@gmail.com>
2025-05-29 16:52:24 +0500

    _documenttx file 2

--- next.config.js
+++ next.config.js
@@ -7,34 +7,13 @@ module.exports = withFrameworkConfig(frameworkName, {
     domains: ['rafraf.com', 's3.me-south-1.amazonaws.com'],
   },
   reactStrictMode: true,
-  experimental: {
-    streaming: true,
-    staticPageGenerationTimeout: 120,
-  },
-  env: {
-    NODE_OPTIONS: '--max-old-space-size=4096',
-  },
-  webpack: (config, { dev, isServer }) => {
+  webpack(config, { dev }) {
     if (dev) {
       config.watchOptions = {
         poll: 1000,
         aggregateTimeout: 300,
       };
     }
-    config.optimization.splitChunks = {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 100000,
-      cacheGroups: {
-        default: false,
-        vendors: false,
-        commons: {
-          name: 'commons',
-          chunks: 'all',
-          minChunks: 2,
-        },
-      },
-    };
     return config;
   },
 });
